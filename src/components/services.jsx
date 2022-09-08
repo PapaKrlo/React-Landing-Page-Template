51fe2de26637aa4a44cdbd89f3b5834acf5a10e2
vcasteline <vcasteline@ufl.edu>
2022-09-08 17:40:08 -0500

    seccion-prestamos

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,20 +1,23 @@
+import image from "../images/100percent-prestamo.png"
+import "../my-styles.css"
 export const Services = (props) => {
   return (
     <div id='services' className='text-center'>
-      <div className='container'>
+      <div className='container '>
         <div className='section-title'>
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <img 
+          className="wide-image"
+          src={image}
+          alt="Imagen prestamo"
+          />
+      
         </div>
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className='col-md-4'>
                   {' '}
-                  <i className={d.icon}></i>
+                  <img className="icon-image" src={d.icon}/>
                   <div className='service-desc'>
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
